20bd266119d390d788e821b1b375982047f2bdb2
chelsea <chelsea.guan99@gmail.com>
2024-06-06 23:54:05 +0200

    Updated output files

--- benchmark/run_Mahdavi_Parallelized.sh
+++ benchmark/run_Mahdavi_Parallelized.sh
@@ -1,65 +1,7 @@
-./benchmark all -m 9 -n 64 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 128 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 256 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 8 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 16 -t 5 -b 1024 -s 1 -l &&
 ./benchmark all -m 9 -n 32 -t 5 -b 1024 -s 1 -l &&
 ./benchmark all -m 9 -n 64 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 128 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 256 -t 5 -b 1024 -s 1 -l &&
-
-./benchmark all -m 5 -n 8 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 16 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 32 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 64 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 128 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 256 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 512 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 1024 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 2048 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 5 -n 4096 -t 3 -b 1024 -s 1 -l &&
-
-./benchmark all -m 6 -n 8 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 16 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 32 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 64 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 128 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 256 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 512 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 1024 -t 2 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 1024 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 1024 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 1024 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 2048 -t 3 -b 1024 -s 1 -l &&
-./benchmark all -m 6 -n 4096 -t 3 -b 1024 -s 1 -l &&
-
-./benchmark all -m 7 -n 4096 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 7 -n 4096 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 7 -n 4096 -t 4 -b 1024 -s 1 -l &&
-
-./benchmark all -m 8 -n 8 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 16 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 32 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 64 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 128 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 256 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 512 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 1024 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 2048 -t 4 -b 1024 -s 1 -l &&
-./benchmark all -m 8 -n 4096 -t 4 -b 1024 -s 1 -l &&
-
-./benchmark all -m 9 -n 8 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 16 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 32 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 8 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 16 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 32 -t 5 -b 1024 -s 1 -l &&
 ./benchmark all -m 9 -n 64 -t 5 -b 1024 -s 1 -l &&
 ./benchmark all -m 9 -n 128 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 256 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 64 -t 5 -b 1024 -s 1 -l &&
 ./benchmark all -m 9 -n 128 -t 5 -b 1024 -s 1 -l &&
 ./benchmark all -m 9 -n 256 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 64 -t 5 -b 1024 -s 1 -l &&
-./benchmark all -m 9 -n 128 -t 5 -b 1024 -s 1 -l &&
 ./benchmark all -m 9 -n 256 -t 5 -b 1024 -s 1 -l
